021b28b3d3bffaa616fdb526b7bb073eb8b427f3
elpe2005 <epedersen2005@gmail.com>
2022-10-05 11:52:26 +0200

    Klar med uppgift 3.15

--- Kapitel 3 uppgift 3.15/Program.cs
+++ Kapitel 3 uppgift 3.15/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+namespace Uppgift313
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen? Det måste vara i hela timmar.");
+            int timmar = int.Parse(Console.ReadLine());
+            int kr = (timmar * 80);
+
+            if (kr <= 950)
+            {
+                Console.WriteLine(kr);
+            }
+            else
+            {
+                Console.WriteLine("Du har kört för länge.");
+            }
+        }
+    }
+}
